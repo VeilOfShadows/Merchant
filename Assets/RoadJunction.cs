8e6267ef952beb5aef7d60a730351e2b91b5d39c
ShadowVeil <nathanchadlindeman99@gmail.com>
2024-01-30 10:31:27 +0000

    Cinemachine Dolly Tracks

--- Assets/RoadJunction.cs
+++ Assets/RoadJunction.cs
@@ -6,6 +6,8 @@ using UnityEngine.Splines;
 public class RoadJunction : MonoBehaviour
 {
     public SplineContainer splineContainer;
+    public SplineContainer splineContainer2;
+    public List<GameObject> cameras = new List<GameObject>();
 
     //public List<int> roads = new List<int>();
 
@@ -16,13 +18,17 @@ public class RoadJunction : MonoBehaviour
         if (other.CompareTag("Player")) 
         {
             Debug.Log("SWITCHING ROAD");
-            if (other.GetComponent<PlayerControls>().currentRoad == splineContainer.Splines[1])
+            if (other.GetComponent<PlayerControls>().currentRoad == splineContainer.Splines[0])
+            {
+                other.GetComponent<PlayerControls>().currentRoad = splineContainer2.Splines[0];
+                cameras[1].SetActive(true);
+                cameras[0].SetActive(false);
+            }
+            else if(other.GetComponent<PlayerControls>().currentRoad == splineContainer2.Splines[0])
             {
                 other.GetComponent<PlayerControls>().currentRoad = splineContainer.Splines[0];
-            }
-            else if(other.GetComponent<PlayerControls>().currentRoad == splineContainer.Splines[0])
-            {
-                other.GetComponent<PlayerControls>().currentRoad = splineContainer.Splines[1];
+                cameras[0].SetActive(true);
+                cameras[1].SetActive(false);
             }
 
         }
