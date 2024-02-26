e95fab8bfd055c7c9b3f3fc23cb528e8698908bf
VeilOfShadows <nathanchadlindeman99@gmail.com>
2024-02-26 13:26:23 +0000

    Interactable Tween

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.VFX;
 
 public class Interactable : MonoBehaviour
 {
     public ItemObject item;
     public int amount;
     public Inventory playerInventory;
+    public Transform interactableObject;
     public GameObject vfx;
-    public GameObject mesh;
+    //public GameObject mesh;
     public Collider collider;
+    Tween scaleTween;
+    public float tweenSpeed = .4f;
 
     public void OnMouseDown()
     {
@@ -19,7 +24,29 @@ public class Interactable : MonoBehaviour
 
         collider.enabled = false;
         playerInventory.AddItem(item.data, amount);
-        vfx.SetActive(false);
-        mesh.SetActive(false);
+        //vfx.SetActive(false);
+        
+        if (scaleTween != null && scaleTween.IsActive() && scaleTween.IsPlaying())
+        {
+            scaleTween.Kill();
+        }
+        Vector3 originalScale = interactableObject.transform.localScale;
+        //Transform transform = mesh.transform.parent;
+        //interactableObject.localScale = Vector3.one;
+        scaleTween = interactableObject.DOPunchScale(originalScale * 1.1f, .4f).OnComplete(() => {
+            scaleTween = interactableObject.DOScale(Vector3.one * .1f, tweenSpeed).OnUpdate(()=>
+            {
+                interactableObject.localPosition = Vector3.zero;
+            }
+            ).OnComplete(() => 
+            {
+                interactableObject.gameObject.SetActive(false);
+                vfx.SetActive(false);
+                interactableObject.localScale = originalScale; 
+            });        
+        });
+
+        
+        //mesh.SetActive(false);
     }
 }
