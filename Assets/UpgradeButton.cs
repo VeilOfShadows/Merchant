a0c8eb66dc527dcf97a29ca042230fb6cf640fe4
VeilOfShadows <nathanchadlindeman99@gmail.com>
2024-03-27 19:28:19 +0000

    Upgrade UI

---
+++ Assets/UpgradeButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public bool purchased = false;
+    public UpgradeObject upgrade;
+    public UpgradeButton nextUpgrade;
+    public Image buttonIcon;
+    public Image icon;
+    public Color green;
+    public Color grey;
+    public Color red;
+
+    public void PurchaseUpgrade() {
+        if (purchased)
+        {
+            return;
+        }
+        
+        if (upgrade != null)
+        {
+            buttonIcon.color = green;
+            GetComponent<Button>().interactable = false;
+            if (nextUpgrade != null)
+            {
+                nextUpgrade.GetComponent<Button>().interactable = true;
+                nextUpgrade.icon.color = grey;
+            }
+            upgrade.PerformAction();
+        }
+    }
+}
